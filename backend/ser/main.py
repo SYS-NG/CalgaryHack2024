aeb30b95ffacf8a8d58d7d45c110f080e89062a0
Luke Son <flare2930@gmail.com>
2024-02-17 14:13:24 -0700

    ser directory created

---
+++ backend/ser/main.py
@@ -0,0 +1,2 @@
+# Following https://github.com/MeidanGR/SpeechEmotionRecognition_Realtime/blob/main/2_model.ipynb to train LSTM model
+# Model train based on https://zenodo.org/records/1188976#.X4sE0tDXKUl
